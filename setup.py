aabba031fee09cdcbf88d8cea207354625e46255
SungHyeon Oh <osh@robotis.com>
2023-03-16 09:35:44 +0900

    Bump commits
    Signed-off-by: SungHyeon Oh <osh@robotis.com>

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     #   py_trees-<version>.js (and version variable therein)
     #   py_trees-<version.css
     #   index.html.
-    version='5.0.0',  # also package.xml
+    version='5.1.0',  # also package.xml
     packages=find_packages(exclude=['tests*', 'docs*']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
